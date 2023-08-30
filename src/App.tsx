1bcb16a184f0cef21b8e6af88d1f4a3d6fd0f180
kimdonggu42 <kdonggoo44@gmail.com>
2023-08-30 20:04:54 +0900

    style: Global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import GlobalStyle from './assets/styles/globalStyle';
 import IssueList from './pages/IssuesList';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <IssueList />
     </div>
   );
---
+++ src/assets/styles/globalStyle.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  a,
+  a:hover,
+  a:focus {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
