bd36d41368e1f7f1e68d10c072713d56dac1997c
kimdonggu42 <kdonggoo44@gmail.com>
2023-08-30 15:47:52 +0900

    :wrench: Chore: acees token 환경 변수 설정 및 gitignore 처리
    acees token 환경 변수 설정 및 gitignore 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState<any>([]);
+
+  const getData = async () => {
+    const res = await axios.get(
+      `https://api.github.com/repos/facebook/react/issues?state=open&sort=comments&page=1`,
+
+      {
+        headers: {
+          Accept: 'application/vnd.github+json',
+        },
+      },
+    );
+    setData(res.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
+
   return <div className="App"></div>;
 }
 
