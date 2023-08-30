314578748e0e5f250b02197a428e9bb2597f97dd
kimdonggu42 <kdonggoo44@gmail.com>
2023-08-30 21:12:20 +0900

    style: IssuesList 레이아웃 설정

--- src/components/IssueItem.tsx
+++ src/components/IssueItem.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
-function IssueItem({ issue }: any) {
+export default function IssueItem({ issue }: any) {
   const navigate = useNavigate();
 
   const moveDetailPage = () => {
@@ -12,7 +13,45 @@ function IssueItem({ issue }: any) {
     });
   };
 
-  return <div onClick={moveDetailPage}>{issue.title}</div>;
+  return (
+    <IssueItemWrap onClick={moveDetailPage}>
+      <InfoArea>
+        <TitleArea>
+          <div>{issue.number}</div>
+          <div>{issue.title}</div>
+        </TitleArea>
+        <DateArea>
+          <div>작성자: {issue.user.login}</div>
+          <div>작성일: {issue.created_at}</div>
+        </DateArea>
+      </InfoArea>
+      <CommetArea>코멘트: {issue.comments}</CommetArea>
+    </IssueItemWrap>
+  );
 }
 
-export default IssueItem;
+const IssueItemWrap = styled.li`
+  display: flex;
+  border: 1px solid red;
+`;
+
+const InfoArea = styled.div`
+  width: 80%;
+
+  border: 1px solid blue;
+`;
+
+const TitleArea = styled.div`
+  display: flex;
+`;
+
+const DateArea = styled.div`
+  display: flex;
+`;
+
+const CommetArea = styled.div`
+  display: flex;
+  align-items: center;
+  width: 20%;
+  font-size: 0.9rem;
+`;
--- src/pages/IssuesList.tsx
+++ src/pages/IssuesList.tsx
@@ -1,19 +1,20 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
 import IssueItem from '../components/IssueItem';
 import { useGetIssues } from '../hooks/useGetIssues';
 import { useInfiniteScroll } from '../hooks/useInfiniteScroll';
 import wantedAd from '../assets/images/wanted_ad.png';
 
-function IssueList() {
+export default function IssueList() {
   const { issues, loading, page, getIssues } = useGetIssues();
   const { targetRef } = useInfiniteScroll(() => {
     getIssues();
   }, page);
 
-  // console.log(issues);
+  console.log(issues);
 
   return (
-    <div>
+    <IssuesContainer>
       {issues.map((issue: any, index: number) => {
         const showAd = (index + 1) % 4 === 0;
         return (
@@ -24,8 +25,14 @@ function IssueList() {
         );
       })}
       {loading ? 'Loading...' : <div ref={targetRef}>Ref</div>}
-    </div>
+    </IssuesContainer>
   );
 }
 
-export default IssueList;
+const IssuesContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+
+  font-size: 1rem;
+`;
